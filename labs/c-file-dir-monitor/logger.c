145b6864289f887b5237d1df20952849c3ecbbed
Paperman2298 <joel7164@gmail.com>
2021-11-11 03:39:20 +0000

    Finished c-file-dir-monitor

--- labs/c-file-dir-monitor/logger.c
+++ labs/c-file-dir-monitor/logger.c
@@ -1,23 +1,95 @@
 #include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <syslog.h>
+#include <stdbool.h>
 #include "logger.h"
 
-int initLogger(char *logType) {
+bool flag=false;
+
+int initLogger(char *logType) { // Checks if the logType is valid
     printf("Initializing Logger on: %s\n", logType);
+
+    if (strcmp(logType, "syslog") == 0){
+        flag = true;
+    }else if (strcmp(logType, "stdout") == 0 || strlen(logType) == 0){
+        flag = false;
+    }else{
+        printf("LogType cannot be understood, try a different argument. \n");
+    }
     return 0;
 }
 
 int infof(const char *format, ...) {
+    va_list args; // Object type suitable for holding the information needed by the macros
+    va_start(args, format);
+
+    if(flag){
+        vsyslog(LOG_NOTICE, format, args); // Normal, but significant, condition
+        closelog();
+    }else{
+        vprintf(format, args);
+    }
+
+    printf("\n");
+
+    va_end(args);
+
     return 0;
 }
 
 int warnf(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+
+    if(flag){
+        vsyslog(LOG_WARNING, format, args); // Warning conditions
+        closelog();
+    }else{
+        vprintf(format, args);
+    }
+
+    printf("\n");
+
+    va_end(args);
+
     return 0;
 }
 
 int errorf(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+
+    if(flag){
+        vsyslog(LOG_ERR, format, args); // Error conditions
+        closelog();
+    }else{
+        vprintf(format, args);
+    }
+
+    printf("\n");
+
+    va_end(args); 
+
     return 0;
 }
+
 int panicf(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+
+    if(flag){
+        vsyslog(LOG_CRIT, format, args); // Critical conditions
+        closelog();
+    }else{
+        vprintf(format, args);
+    }
+
+    printf("\n");
+
+    va_end(args);
+
     return 0;
 }
 
--- labs/c-file-dir-monitor/monitor.c
+++ labs/c-file-dir-monitor/monitor.c
@@ -1,7 +1,84 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/inotify.h>
+#include <unistd.h>
+#include <stdbool.h>
 #include "logger.h"
 
-int main(){
+int main(int argc, char **argv){
     // Place your magic here
+
+    if(argc < 2){
+        printf("Missing arguments; \n");
+        printf("Try using ./monitor /someDirectory");
+        return 404;
+    }
+
+    char* current_path = argv[1];
+    char arr[10000];
+    int file = inotify_init(); // Returns a file descriptor associated with a new inotify event queue.
+
+    if(file < 0){
+        printf("Not able to initialize notify, check if file exists. \n");
+        return 404;
+    }
+
+    int watchList = inotify_add_watch(file, current_path, IN_CREATE | IN_MOVED_FROM | IN_MOVED_TO | IN_DELETE);
+    // Each item in the watch list specifies the pathname of a file or directory and the event
+
+    if(watchList > 0){
+        printf("Starting file: %s. \n", current_path);
+    }else{
+        printf("Couldn't add %s to watch list. \n", current_path);
+    }
+
+    int i;
+    bool flag = true;
+    while(flag){
+        i = 0;
+        int r = read(file, arr, 10000);
+
+        if(r < 0){
+            printf("Couldn't read file. \n");
+            break;
+        }
+
+        while(i < r){
+            struct inotify_event *event = (struct inotify_event*) &arr[i]; // Describes a filesystem event
+            if(event -> len){
+                if(event -> mask & IN_CREATE){ // Check for creation
+
+                    // Check if its file or directory
+                    if(event -> mask & IN_ISDIR){ // Subject of this event is a directory
+                        infof("[Directory - Create]: %s\n", event -> name);
+                    }else{
+                        infof("[File - Create]: %s\n", event -> name);
+                    }
+                }else if(event -> mask & IN_MOVED_FROM){ // Check for movement from
+                    if(event -> mask & IN_ISDIR){
+                        infof("[Directory - Rename]: %s\n", event -> name);
+                    }else{
+                        infof("[File - Rename]: %s\n", event -> name);
+                    }
+                }else if(event -> mask & IN_MOVED_TO){ // Check for move to
+                    if(!(event -> mask & IN_ISDIR)){
+                        infof("-> %s\n", event -> name);
+                    }
+                }else if(event -> mask & IN_DELETE){ // Check for delete
+                    if(event -> mask & IN_ISDIR){
+                        infof("[Directory - Removal]: %s\n", event -> name);
+                    }else{
+                        infof("[File - Removal]: %s\n", event -> name);
+                    }
+                }
+
+                i += event -> len;
+            }
+        }
+    }
+
+    inotify_rm_watch(file, watchList); // Remove an existing watch from an inotify instance
+    close(file);
+
     return 0;
 }
